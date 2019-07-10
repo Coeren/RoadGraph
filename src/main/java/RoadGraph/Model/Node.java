8280f4f7bfcb315a64ed9a0a07a2b0a7c0af4d8b
Ilya A. Tereshchenko <Ilya.Tereshchenko@redsys.ru>
2019-07-10 16:23:31 +0700

    Road graph: left, right anf straight links are separated

--- src/main/java/RoadGraph/Model/Node.java
+++ src/main/java/RoadGraph/Model/Node.java
@@ -1,19 +1,35 @@
 package RoadGraph.Model;
 
 import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 @XmlType(name = "NodeType")
 public class Node {
     @XmlAttribute(name = "Name")
     @XmlID
     private String name;
+//    @XmlElement(name = "RoadTo")
+//    @XmlIDREF
+//    private Collection<Node> roadsTo = new LinkedList<>();
+
+    @XmlElementWrapper(name = "Left")
     @XmlElement(name = "RoadTo")
     @XmlIDREF
-    private Collection<Node> roadsTo = new LinkedList<>();
+    private List<Node> leftRoads = new ArrayList<>();
+    @XmlElementWrapper(name = "Straight")
+    @XmlElement(name = "RoadTo")
+    @XmlIDREF
+    private List<Node> straightRoads = new ArrayList<>();
+    @XmlElementWrapper(name = "Right")
+    @XmlElement(name = "RoadTo")
+    @XmlIDREF
+    private List<Node> rightRoads = new ArrayList<>();
 
     public Node() { this(""); }
-    public Node(String name) { this.name = name; }
-    void linkTo(Node target) { roadsTo.add(target); }
+
+    Node(String name) { this.name = name; }
+    void linkTo(Node target) { straightRoads.add(target); }
 }
